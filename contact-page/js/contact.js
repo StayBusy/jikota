95c6771412b124ee72dd447ffca9fee01c41c08d
Albert Okon <okonalbert@yahoo.com>
2018-02-05 11:29:17 +0000

    report-a-bug page

--- contact-page/js/contact.js
+++ contact-page/js/contact.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
         var textLength = $("#exampleMessage").length;
         //if value length of the string in textarea is less than 30
         if (textLength < 30) {
-            //append a new paragraph below the textarea showing an error message
-            $("#exampleMessage").after("<p class='float-right'>Your Message has less than 30 characters</p>");
+            //write an error message in the small element
+            $("#msgCount").html("Your Message has less than 30 characters");
         }
     });
 
